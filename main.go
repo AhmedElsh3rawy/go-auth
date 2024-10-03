a397cc05d429cf49e2d30d399b4345730d4a183a
Ahmed <ahmedelsharawy117@gmail.com>
2024-10-03 21:40:24 +0300

    chore: creating server

--- main.go
+++ main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/joho/godotenv"
 )
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	fmt.Println("go-auth")
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", getHello)
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	log.Println("[server]: running on localhost:8080")
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Printf("Error starting server: %v\n", err)
+		return
+	}
 }
