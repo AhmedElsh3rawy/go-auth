30a14bf6f84370d35cce31b52f06fa7dc3a7abe7
Ahmed <ahmedelsharawy117@gmail.com>
2024-10-06 01:00:02 +0300

    start login logic

--- handler/user.go
+++ handler/user.go
@@ -12,28 +12,26 @@ import (
 	"github.com/google/uuid"
 )
 
-type CreatedUser struct {
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
 func Register(w http.ResponseWriter, r *http.Request) {
-	var u CreatedUser
-	err := json.NewDecoder(r.Body).Decode(&u)
+	var body struct {
+		Username string `json:"username"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&body)
 	if err != nil {
 		fmt.Println("error while decoding request body")
 		return
 	}
 
 	ctx := context.Background()
-	_, err = database.Query.GetUser(ctx, u.Email)
+	_, err = database.Query.GetUser(ctx, body.Email)
 	if err == nil {
 		http.Error(w, "User is already registered", http.StatusBadRequest)
 		return
 	}
 
-	hash, err := helpers.HashPassword([]byte(u.Password))
+	hash, err := helpers.HashPassword([]byte(body.Password))
 	if err != nil {
 		fmt.Println("error hashing password")
 		return
@@ -41,8 +39,8 @@ func Register(w http.ResponseWriter, r *http.Request) {
 
 	err = database.Query.CreateUser(ctx, users.CreateUserParams{
 		ID:       uuid.New(),
-		Username: u.Username,
-		Email:    u.Email,
+		Username: body.Username,
+		Email:    body.Email,
 		Password: string(hash),
 	})
 	if err != nil {
@@ -55,8 +53,30 @@ func Register(w http.ResponseWriter, r *http.Request) {
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
-	// get the body email and password
-	// compare passwords
+	var body struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		fmt.Println("error while decoding request body")
+		return
+	}
+
+	ctx := context.Background()
+	u, err := database.Query.GetUser(ctx, body.Email)
+	if err != nil {
+		fmt.Println("error quering database")
+		return
+	}
+
+	err = helpers.ComparePasswords([]byte(u.Password), []byte(body.Password))
+	if err != nil {
+		http.Error(w, "Invalid email or password", http.StatusBadRequest)
+		return
+	}
+	w.Write([]byte("All good!"))
 	// generate jwt token
 	// response
 }
