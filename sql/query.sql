bd89fd3a9a351b9bbe3e5679e390957f87a016bc
Ahmed <ahmedelsharawy117@gmail.com>
2024-10-09 22:49:38 +0300

    .

--- sql/query.sql
+++
@@ -1,9 +0,0 @@
--- name: GetUser :one
-SELECT * FROM users WHERE email = $1 LIMIT 1;
-
--- name: CreateUser :exec
-INSERT INTO users (
-  id, username, email, password
-) VALUES (
-  $1, $2, $3, $4
-);
